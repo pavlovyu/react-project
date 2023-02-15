d280d175769a5549aa8d51801aa6ed6f61605708
Юрий Павлов <pavlovyuriypho@gmail.com>
2023-02-15 10:29:16 +0200

    work

---
+++ react-project/src/App.js
@@ -0,0 +1,19 @@
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './Components/Layout'
+import Home from './Components/Home'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
+
+        </Route> 
+      </Routes>
+    </>
+  );
+}
+
+export default App;
